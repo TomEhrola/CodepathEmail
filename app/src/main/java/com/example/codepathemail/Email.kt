a4ad3fb6ed74a05080042eb6af5ac40102af3f36
TomEhrola <tpe3@njit.edu>
2023-09-21 17:14:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/codepathemail/Email.kt
@@ -0,0 +1,10 @@
+package com.example.codepathemail
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String){
+    //,    val date: String
+
+    //val drawable: Int) {
+}
